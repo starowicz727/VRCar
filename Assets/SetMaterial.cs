dfc664a6055726e378af890f5cbe29c5a52a391b
starowicz727 <starowicz727@gmail.com>
2024-03-27 20:51:19 +0100

    configuration panel finished

--- Assets/SetMaterial.cs
+++ Assets/SetMaterial.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class SetMaterial : MonoBehaviour
 {
@@ -24,6 +25,10 @@ public class SetMaterial : MonoBehaviour
         if (other.gameObject.tag == "Player")
         {
             audioCanvas.Play();
+            foreach (GameObject obj in carElements)
+            {
+                obj.GetComponent<MeshRenderer>().material = this.GetComponent<Image>().material;
+            }
         }
     }
 
